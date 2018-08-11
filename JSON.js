69212f441e26ea971b5c97898dd7429d845b8ec2
Sigal Shalit Manor <sigal.shalit.manor@gmail.com>
2018-08-11 18:37:48 +0300

    first commit

---
+++ JSON.js
@@ -0,0 +1,106 @@
+//making mash potatoes with JSON:
+{
+    "name": "Mashed Potatoes",
+    "serves": 4,
+    "calories": 250,
+    "ingredients": [
+      {
+        "name": "water"
+      },
+      {
+        "name": "potatoes",
+        "count": 2
+      },
+      {
+        "name": "butter",
+        "count": 2,
+        "unit": "tablespoons"
+      },
+      {
+        "name": "salt",
+        "count": 0.5,
+        "unit": "teaspoon"
+      },
+      {
+        "name": "pepper",
+        "count": 0.25,
+        "unit": "teaspoons"
+      },
+      {
+          "name": "garlic powder",
+          "count": 2,
+          "unit": "teaspoons"
+      },
+      {
+          "name": "green onions",
+          "count": 3,
+          "unit": "stalks"
+      }
+    ],
+    "directions": [
+      "Cut potatoes into half inch thick slices.",  
+      "Add potatoes to a pot and cover with an inch of water.",
+      "Bring the water to a boil over high heat, then reduce to a simmer and cover.",
+      "Simmer for 20 minutes or until potatoes are fork tender.",
+      "Drain water from potatoes, leaving the potatoes in the original pot.",
+      "Add remaining ingredients to the pot with the potatoes.",
+      "Mash potatoes until they reach the desired consistency.",
+      "Serve immediately, or cover and refrigerate.",
+      "For decoration and extra flavor, chop some green onions and add it to the pot."
+    ],
+    [
+    "serving size": 0.5,
+    "unit": "cup"
+    ]
+};
+
+
+  //JSON document about animals:
+{
+    "name": "animals",
+    "information": 
+    [
+      {
+        "name": "cow",
+        "max weight in kilos": 1361,
+        "living environment": "grassland, forest, pasture",
+        "endangered": "no",
+        "food": "plants, grass, grains, seeds",
+        "diet (in percentages)": [{"plants": 5}, {"grass": 40}, {"grains": 50}, {"seeds": 5}],
+        "animals that eats or preys this animal": "humans, grizzly bears, wolves, tigers, and so on..",
+        "How delicious is the animal, on a scale of 1 to 5?": 5 
+      },
+      {
+        "name": "elephant",
+        "max weight in kilos": 6000,
+        "living environment": "grassland, desert, forest area, swamp",
+        "endangered": "yes",
+        "food": "a variety of leaves and other plant materials",
+        "diet (in percentages)": [{"leaves": 75}, {"plant materials": 25 }],
+        "animals that eats or preys this animal": "lions, humans",
+        "How delicious is the animal, on a scale of 1 to 5?": 1 
+      },
+      {
+        "name": "gray wolf",
+        "max weight in kilos": 45,
+        "living environment": "deserts, grasslands, forests and arctic tundras",
+        "endangered": "yes",
+        "food": "medium or small size animals",
+        "diet (in percentages)": [{"medium size animals": 20}, {"small size animals": 80}],
+        "animals that eats or preys this animal": "mainly humans",
+        "How delicious is the animal, on a scale of 1 to 5?": 1
+      },
+      {
+        "name": "squirel",
+        "max weight in kilos": 0.4,
+        "living environment": "forest",
+        "endangered": "no",
+        "food": "plants, seeds, mushrooms, small insects and eggs",
+        "diet (in percentages)": [{"plants": 60}, {"seeds": 5}, {"mushrooms": 15}, {"small insects": 7}, {"eggs": 3}],
+        "animals that eats or preys this animal": "hawk, owl, coyote, snake and so on...",
+        "How delicious is the animal, on a scale of 1 to 5?": 1  
+      }
+    ]
+};
+
+
